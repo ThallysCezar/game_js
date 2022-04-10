f52a77d28f97295f30481e03676c598a7226a62a
Thallys <thalys_cezar@hotmail.com>
2022-04-10 12:44:15 -0300

    refactor: reconfigurando css e o javascript, part2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,29 @@
+//criando as variáveis e pegando o valor delas do html
 let character = document.getElementById('character');
 let block = document.getElementById('block');
 
+
+// o efeito de pular
 function jump() {
-    character.classList.add('animated');
+
+    if(character.classList != 'animated'){
+        character.classList.add('animated');
+    }
+
     setTimeout(function(){
         character.classList.remove('animated')
     }, 500);
 }
+
+
+//verificar a morte, se o bloco entrou em contato com o character
+let verfMorte = setInterval(function(){
+    let characterTop = parseInt (window.getComputedStyle(character).getPropertyValue('top'));
+    let blockLeft = parseInt (window.getComputedStyle(block).getPropertyValue('left'));
+
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert("Você perdeu!!!")
+    }
+}, 10);
